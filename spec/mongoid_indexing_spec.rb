b47902395d54a966846463eafaff1d5457d5978b
Kristian Mandrup <kmandrup@gmail.com>
2012-09-23 15:23:21 +0200

    ready2rock

--- spec/mongoid_indexing_spec.rb
+++ spec/mongoid_indexing_spec.rb
@@ -1,7 +1,5 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'spec_helper'
 
 describe "MongoidIndexing" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
-  end
+  pending 'Tested in another project  - no specs for now :P'
 end
